1a216030888f707c3087c13e237210049d4ef5c1
MdTasib <ohidulalam442@gmail.com>
2021-08-05 12:19:45 +0600

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-    </div>
+        <Switch>
+          <Route path="/about">
+
+          </Route>
+          <Route path="/users">
+
+          </Route>
+          <Route path="/">
+
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
