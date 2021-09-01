77995f51a6476c0c4805da8c20b1e15ddee8ed9c
MdTasib <ohidulalam442@gmail.com>
2021-09-01 11:21:26 +0600

    home page database load book

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import fakeData from '../fakeData';
 import Product from '../product/Product';
 import { Row } from 'react-bootstrap';
+import spinner from '../../images/loding.gif';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/books')
+      .then(response => response.json())
+      .then(data => setBooks(data));
+  }, []);
+
   return (
     <Row xs={1} md={4} className="g-4 justify-content-md-center">
       {
-        fakeData.map(data => <Product key={data.id} product={data} />)
+        books.length > 0
+          ?
+          books.map(data => <Product key={data._id} product={data} />)
+          :
+          <img src={spinner} alt='Spinner' />
       }
     </Row>
   );
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -5,13 +5,10 @@ const Product = ({ product }) => {
   return (
     <Col xs={12} md={4} sm={6}>
       <Card>
-        <Card.Img variant="top" src={product.img} />
+        <Card.Img variant="top" src={product.imageUrl} />
         <Card.Body>
           <Card.Title>{product.name}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
+          <Card.Text>{product.author}</Card.Text>
           <div className="d-flex justify-content-between align-items-end">
             <Card.Title>${product.price}</Card.Title>
             <Button variant="dark">Buy Now</Button>
