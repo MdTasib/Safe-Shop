3a791f44c3647ad4102573641cc414cf5e0016af
MdTasib <ohidulalam442@gmail.com>
2021-09-02 13:16:38 +0600

    project done

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,13 +1,35 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import { Form, FormControl, Button, } from 'react-bootstrap';
 
 const SearchBox = () => {
+  // const [filter, setFilter] = useState('');
+  // const [books, setBooks] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/books')
+  //     .then(response => response.json())
+  //     .then(data => setBooks(data));
+  // }, []);
+
+  // const searchProduct = (event) => {
+  //   setFilter(event.target.value);
+  // };
+
+  // let dataSearch = books.filter(item => {
+  //   return Object.keys(item).some(key =>
+  //     item[key].toString().toLowercase().includes(filter.toString().toLowerCase())
+  //   )
+  // });
+
   return (
     <Form className="d-flex w-50 m-auto py-3 mb-4">
       <FormControl
         type="search"
         placeholder="&#128269; Search Here"
         aria-label="Search"
+      // value={filter}
+      // onChange={searchProduct.bind(this)}
       />
       <Button variant="dark">Search</Button>
     </Form>
